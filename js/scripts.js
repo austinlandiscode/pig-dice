c07bd2f102052577f27ee5acd234f2123d1dc04e
Sage Sypert <sage.sypert@gmail.com>
2021-01-27 11:09:24 -0800

    add if statements to business and ui logic
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>
    
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    
    Co-Authored by: Olha Hizhytska <olgainfotech@gmail.com>
    
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
 function Player(name) {
   this.name = name;
   this.score = 0;
-  this.turnScore = 0;
+  this.updatedScore = 0;
 }
 
 
@@ -16,17 +16,32 @@ Player.prototype.diceRoll = function() {
   //bring in dice object - happens on call
   //capture the orginal score value (let orgScore = score;)
   //roll a random range between 1-6
-  //branch if(1) {
-    //score = orgScore;
-    //break
-  //}
-  //branch if(2-6) {
-    //add number
-    //return
-  //
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  let hold = false;
+  if(hold === true) {
+    score = updatedScore
+  } else {
+    player.diceRoll()
+  }
+
+
+
+
+
+  if (randomNumber1 === 1) {
+    alert("You rolled a 1! Sorry, your turn is over.");
+    break;
+  } else {
+  updatedScore += randomNumber1;
+  prompt("roll again?")
+   
+    player.diceRoll()
+    
+  }
+    player.diceRoll()
+    firstTime = false;
 };
 
-let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 
 //ui logic
@@ -34,9 +49,12 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 // Creates two players
 let playerOne = new Player("Player One");
 let playerTwo = new Player("Player Two");
-// player1 or player2
-// if(player == playerOne)  {
-//   player = playerTwo;
-// }
+
+player1 or player2
+
+if(player == playerOne)  {
+  player = playerTwo;
+}
 
 player.diceRoll;
+
