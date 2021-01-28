693c184ddcfaae3c612ff8126543d281bf3c4637
Sage Sypert <sage.sypert@gmail.com>
2021-01-27 16:00:47 -0800

    add additional if else statements
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>
    
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    
    Co-Authored by: Olha Hizhytska <olgainfotech@gmail.com>
    
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,71 @@
 // Business Logic
 
+
+//*NOTES*//
+
 // Create player constructor
 // Create object for player one and player two 
 // Container with incrementing or zero score for each roll/ branching
+
 
 // Player Object Constructor
 function Player(name) {
   this.name = name;
   this.score = 0;
   this.updatedScore = 0;
+  this.hasRolled = true;
+  this.firstTime =true;
 }
 
 
 Player.prototype.diceRoll = function() {
-  //bring in dice object - happens on call
-  //capture the orginal score value (let orgScore = score;)
-  //roll a random range between 1-6
-  let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-  let hold = false;
+  
+  if(this.firstTime) {
+    let originalScore = this.score;
+    this.firstTime = false;
+  }
+    updatedScore = score
+  
+
   if(hold === true) {
-    score = updatedScore
+    score = updatedScore;
+    return this.score;
+  
   } else {
-    player.diceRoll()
-  }
-
-
-
-
-
-  if (randomNumber1 === 1) {
-    alert("You rolled a 1! Sorry, your turn is over.");
-    break;
-
-
-  } else {
-  updatedScore += randomNumber1;
-  prompt("roll again?")  //replace
-  prompt("hold?")  //replace
-  let hasRolled = true;
-    player.diceRoll()
+    // player.diceRoll()
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
     
-  }
-    player.diceRoll()
-    firstTime = false;
-    let hasRolled = true;
+    if (randomNumber1 === 1) {
+      alert("You rolled a 1! Sorry, your turn is over.");
+      firstTime = true;
+      this.score;
+      
+    } else {
+      updatedScore += randomNumber1; //4
+      player.hasRolled = true;
+      // prompt("roll again?");  //replace
+      // prompt("hold?");  //replace
+      if ("roll again === true;") {
+        // this.diceRoll();
+        // hold = false;
+      } else {
+        score = updatedScore;
+        hold = true;
+        this.diceRoll();
+        firsttime = true;
+      };
+    };
+  };
 };
+  
+
+    
+  
+//     hold = false; //* Yet to declare
+//     player.diceRoll();
+//   };
+//     firstTime = false;
+// };
 
 
 
@@ -52,16 +73,19 @@ Player.prototype.diceRoll = function() {
 
 // Creates two players
 
-let currentPlayer = playerOne;
-player1 or player2
+// let currentPlayer = playerOne;
+// player1 or player2
 
 
 function playerSwitch()  {
-  if(currentPlayer == playerOne)  {
+  let currentPlayer = playerOne;
+  if(currentPlayer === playerOne)  {
     currentPlayer = playerTwo;
-} else {
-  currentPlayer = playerOne;
-}
+    console.log(currentPlayer);
+  } else {
+    currentPlayer = playerOne;
+    console.log(currentPlayer);
+  }
 }
 
 
@@ -69,27 +93,46 @@ function playerSwitch()  {
 
 $(document).ready(function() {
 
-  let playerOne = new Player("Player One");
-  let playerTwo = new Player("Player Two");
+  let playerOne = new Player("playerOne");
+  let playerTwo = new Player("playerTwo");
   let currentPlayer = playerOne;
+  
+  //hide hold button
+  if(currentPlayer.hasRolled) {
+    $("#roll").show();
+    $("#hold").show();
+  } else {
+    $("#roll").hide();
+    $("#hold").hide();
+  }
   
   $("#roll").on("click", function(event){
     event.preventDefault();
-    currentPlayer.diceRoll()
-    
+    // currentPlayer.diceRoll()
+    console.log("clicked roll");
   });
 
   $("#hold").on("click", function(event){
     event.preventDefault();
-    
-    
+    playerSwitch();
+    console.log("clicked hold");
   });
 });
 
 //function for program will always start at player 1
 //switches back and forth when a player rolls 1 or holds.
 
-if whatever (player !== 1) {
+// function PlayerDatabase () {
+//   this.contacts ={};
+//   this.currentId = 0;
+// }
 
-  $("#hold").show();
-}
+// PlayerDatabase.prototype.addPlayer = function(player) {
+//   player.id = this.assignId();
+//   this.players[player.id] = player;
+// }
+
+// PlayerDatabase.prototype.assignId() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
